914b8c7faf5ae67f7f49039035aeacb58a12aaab
seabass6969 <cephas8080@outlook.com>
2022-02-25 18:16:15 +0000

    2 files modified

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,8 @@
 <script>
 import json from '$lib/bazaar'
-for (let i=0; i < json.product.length; i++)
+for (let i=0; i < json.products.length; i++){
+  console.log(json.products[i])  
+}
 </script>
 <h1>test</h1>
+<h1>{json.products[1]}</h1>
